0cf0a08624e377fde7d6b2c1810cc55099d3481a
udexmickey <jerrymike80@yahoo.com>
2021-05-27 09:41:08 +0100

    first commit

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function index() {
+    return (
+        <div>
+            <h3> Welcome Home Page</h3>
+        </div>
+    )
+}
---
+++ src/Pages/Price/Price.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Price() {
+    return (
+        <div>
+            <h3> Welcome Pricing Page</h3>
+        </div>
+    )
+}
